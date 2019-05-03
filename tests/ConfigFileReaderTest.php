0e7014249586b35f31dc517ab2c8b44f132ed266
Rainer Friederich <rainer@wondrous.ch>
2019-05-03 14:47:37 +0200

    refactor some things into a Helper Class ConfigFileReader. Use this class ion SiteSettings and BaseCommands. Add Tests for the new class

---
+++ tests/ConfigFileReaderTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace wearewondrous\PshToolbelt\Tests;
+
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+use wearewondrous\PshToolbelt\ConfigFileReader;
+use org\bovigo\vfs\vfsStream;
+
+final class ConfigFileReaderTest extends TestCase
+{
+
+  /**
+   * @var vfsStreamDirectory
+   */
+  public $fileSystem;
+
+  public function setUp()
+  {
+    $structure = [
+      'robo.yml' => 'test',
+      'robo.yml.dist' => 'test',
+    ];
+
+    $this->fileSystem = vfsStream::setup('root', null, $structure);
+  }
+
+  public function testGetAcceptableProjectLocalConfigFilenames(): void
+  {
+    $configFileReader = new ConfigFileReader($this->fileSystem->url());
+
+    $this->assertEquals(
+      $configFileReader->getAcceptableProjectLocalConfigFilenames(),
+      ['robo.yml', 'toolbelt.yml']
+    );
+  }
+
+  public function testGetProjectLocalConfigFilename() : void
+  {
+    $configFileReader = new ConfigFileReader($this->fileSystem->url());
+
+    $this->assertEquals(
+      'robo.yml',
+      $configFileReader->getProjectLocalConfigFilename()
+    );
+  }
+
+  public function testGetProjectLocalConfigFilenameWithToolbelt() : void
+  {
+    $structure = [
+      'toolbelt.yml' => 'test',
+      'toolbelt.yml.dist' => 'test',
+    ];
+
+    $localFileSystem = vfsStream::setup('root', null, $structure);
+
+    $configFileReader = new ConfigFileReader($localFileSystem->url());
+
+    $this->assertEquals(
+      'toolbelt.yml',
+      $configFileReader->getProjectLocalConfigFilename()
+    );
+  }
+}

0ffbb49dfa0e55fcf07a9267e6785c47bd729708
Rainer Friederich <rainer@wondrous.ch>
2019-07-02 17:49:18 +0200

    fix getEvn static

--- src/SiteSettings.php
+++ src/SiteSettings.php
@@ -75,8 +75,19 @@ class SiteSettings {
     $this->roboConfig       = $this->configFileReader->getRoboConfig();
   }
 
-  protected function getEnv(string $variable) : ?string {
-    return $this->pshConfig->variable($variable, getenv($variable));
+  protected function getEnv(string $variableName) : ?string {
+    $envVariable = getenv($variableName);
+    $pshVariable = $this->pshConfig->variable($variableName);
+
+    if($pshVariable !== NULL) {
+      return $pshVariable;
+    }
+
+    if($envVariable !== FALSE) {
+      return $envVariable;
+    }
+
+    return NULL;
   }
 
   /**
